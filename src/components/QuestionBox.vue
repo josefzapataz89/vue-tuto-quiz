9b2de862388770cd7b4c80e7f89fcf9cecb7e998
Jose Zapata <joferzz1989@gmail.com>
2020-02-29 17:11:49 -0400

    add question

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        {{ currentQuestion.question }}
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      currentQuestion: Object
+    },
+    name: 'QuestionBox',
+  };
+</script>
+
+<style scoped>
+
+</style>
